fdabc631df2a119ed7b9feba5eece3d3084d511b
Luara <luararaggio@hotmail.com>
2024-05-15 20:26:33 -0300

    inter 2 exam_rank3

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,17 +1,48 @@
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+size_t		ft_strlen(char *str)
 {
-	char	*buf;
-	
-	while ()
-	{
-		bytes_read = read(fd, buf, nbytes);
-	}
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-int	main(void)
+char	*strcpy(char *s1, char *s2)
 {
-	char	*buf;
-	get_next_line(test.txt);
+	int		i;
+
+	i = 0;
+	while (s2[i])
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
+
+
+char	*get_next_line(int fd)
+{
+	static char	buf[BUFFER_SIZE];
+	char	*line;
+
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	line = read(fd, buf, BUFFER_SIZE);
+}
+
+int main(void)
+{
+    int fd;
+    char *s;
+
+   fd = open("test.txt", O_RDONLY);
+   s = get_next_line(fd);
+   printf("%s", s);
+   free(s);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -4,4 +4,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#ifndef BUFFER_SIZE 10
+# define BUFFER_SIZE 10
+#endif
+
+char    *get_next_line(int fd);
+
 #endif
